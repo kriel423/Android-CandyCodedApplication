be2d22f88468f575c84c01813815ddd0039f579d
Aadil <kriel423@gmail.com>
2020-10-12 16:16:18 +0200

    Test in StartTheStoreInformationActivity working

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -87,10 +87,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-                Intent infoIntent = new Intent(MainActivity.this, InfoActivity.class);
-                this.startActivity(infoIntent);
-                return super.onOptionsItemSelected(item);
-        }
+            Intent infoIntent = new Intent(MainActivity.this, InfoActivity.class);
+            this.startActivity(infoIntent);
+            return super.onOptionsItemSelected(item);
+
+    }
 
     // ***
     // TODO - Task 1 - Show Store Information Activity
